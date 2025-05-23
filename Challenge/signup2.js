af4079cf6c890c095879937ec835c90330b30a28
mario chuks <mariochuks20@gmail.com>
2025-05-23 10:01:52 +0100

    visibility toggle addition

--- Challenge/signup2.js
+++ Challenge/signup2.js
@@ -7,3 +7,20 @@ submitButton.addEventListener("click", () => {
     let username = name.value;
     localStorage.setItem('username',username);
 });
+let isVisible = false;
+
+  eyeIcon.addEventListener("click", () => {
+      const passwordInput = document.getElementById("password");
+      const isPassword = passwordInput.type === "password";
+        passwordInput.type = isPassword ? "text" : "password";
+        eyeIcon.src = isPassword ? "icons/eye-on.svg" : "icons/eye-off.svg";
+        eyeIcon.alt = isPassword ? "Hide password" : "Show password";
+    });
+ 
+  eyeIcon2.addEventListener("click", () => {
+      const passwordInput = document.getElementById("password2");
+      const isPassword = passwordInput.type === "password";
+        passwordInput.type = isPassword ? "text" : "password";
+        eyeIcon2.src = isPassword ? "icons/eye-on.svg" : "icons/eye-off.svg";
+        eyeIcon2.alt = isPassword ? "Hide password" : "Show password";
+    });
