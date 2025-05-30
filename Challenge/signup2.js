8d27aa78dd97e72d9163685d319cf3e32530f136
MarioWaruii <mariochuks20@gmail.com>
2025-05-30 21:37:58 +0100

    updates

--- Challenge/signup2.js
+++ Challenge/signup2.js
@@ -2,10 +2,19 @@ const submitButton = document.getElementById('submit-btn');
 // const usernameDisplay = document.getElementById('displayUsername').innerText;
 
 submitButton.addEventListener("click", () => {
+    const password = document.getElementById("password").value;
+    const password2 = document.getElementById("password2").value;
     const name = document.getElementById('username');
     // const password = document.getElementById('')
+    checkPasswords();
+     if (password !== password2) {
+      event.preventDefault();
+     } else {
+        window.location.href = "http://127.0.0.1:5500/Challenge/recreate2.html";
+     }
     let username = name.value;
     localStorage.setItem('username',username);
+
 });
 let isVisible = false;
 
@@ -23,4 +32,18 @@ let isVisible = false;
         passwordInput.type = isPassword ? "text" : "password";
         eyeIcon2.src = isPassword ? "icons/eye-on.svg" : "icons/eye-off.svg";
         eyeIcon2.alt = isPassword ? "Hide password" : "Show password";
-    });
+    }); 
+
+   function checkPasswords() {
+      const password = document.getElementById('password').value;
+      const password2 = document.getElementById('password2').value;
+      const message = document.getElementById('message');
+
+      if (password !== password2) {
+        message.textContent = "Passwords do not match.";
+        return false;
+      }
+
+      message.textContent = "";
+      return true;
+    };
