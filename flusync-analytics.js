43500bfb9f6ea5963a233775e8ebac09a7399b6b
MarioWaruii <mariochuks20@gmail.com>
2025-07-02 17:13:06 +0100

    updates

--- flusync-analytics.js
+++ flusync-analytics.js
@@ -1,58 +1,4 @@
 
-
-const data = {
-    labels: ["item", "Item 2", "Item 3", "Item 4"],
-    datasets: [{
-        data: [19, 66, 87, 34],
-        backgroundColor: ['#febef1'],
-        borderRadius: 5,
-    }],
-    
-};const config = {
-type: 'bar',
-data: data,
-label: 'Data',
-options: {
-    scales: {
-        y: {
-            max: 100,
-            ticks: {
-                callback: value => `${value}%`
-            },
-            display: false,
-            grid: {
-                display: false
-            }
-        },
-        x:{
-            grid:{
-                display: false
-            }
-        }
-    },
-    plugins: {
-        legend: {
-            display: false,
-            position: 'top',
-        },
-        tooltip: {
-            callbacks: {
-                label: context => `${context.raw}%`
-            }
-        }
-    },
-    responsive: true,
-    maintainAspectRatio: false,
-
-
-}
-};
-
-
-new Chart(
-document.getElementById('chart1'),
-config
-);
 const ctx = document.getElementById('PieChartOne')
   const PieChartOne = new Chart(ctx, {
     type: 'doughnut',
@@ -76,6 +22,42 @@ const ctx = document.getElementById('PieChartOne')
         borderWidth: 1
       }]
     },
+    options: {
+      responsive: true,
+      plugins: {
+        legend: {
+          position: 'bottom'
+        },
+        title: {
+          display: false,
+          text: 'My 4-Slot Pie Chart'
+        }
+      }
+    }
+  });
+const ctx2 = document.getElementById('PieChartTwo')
+  const PieChartTwo = new Chart(ctx2, {
+    type: 'doughnut',
+    data: {
+      labels: ['gross', 'salary', 'fixed', 'liquid'],
+      datasets: [{
+        label: 'Sample Data',
+        data: [50, 23, 15, 12], // 4 data slots
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.7)',
+          'rgba(54, 162, 235, 0.7)',    
+          'rgba(255, 206, 86, 0.7)',
+          'rgba(75, 192, 192, 0.7)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)', 
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)'
+        ],
+        borderWidth: 1
+      }]
+       },
     options: {
       responsive: true,
       plugins: {
